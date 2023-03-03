5dbba087acb7c221aad889bf1ae7f57c262dae54
Josh <joshua.medina@sjsu.edu>
2023-03-03 11:06:18 -0800

    updates and clean up

--- index.php
+++ index.php
@@ -14,30 +14,35 @@
   </head>
   <body>
     <div class="parentBox"> <!-- Used for display flex. helps to align boxes on pages to parent-->
-      <div class="brandName mt6 pt ml7"> <!--sets a display box for all company name and title-->
-        <div class="mt7 ml7 pl6">
-          <p class ="intro ma0">LinkMe</p>
-          <p class ="bar mt0 ma0"></p>
-          <p class ="tagLine ma0">Linking Company and Creator</p>
+      <div class = "page1">
+        <div class="brandName mt6 pt ml7"> <!--sets a display box for all company name and title-->
+          <div class="mt7 ml7 pl6">
+            <p class ="intro ma0">LinkMe</p>
+            <p class ="bar mt0 ma0"></p>
+            <p class ="tagLine ma0">Linking Company and Creator</p>
+          </div>
+        <div class="login mt6 pt5 pl3 mh6"> <!--starts the display box that holds the email password and submit button-->
+          <div class = "button mw9">
+            <form action="creatorCompany.php" method="post" class="ml3 mr5 pt3 pb2">
+              <label for="email"> Email:</label>
+              <input class="bt-0-l br-0-l bl-0-l mw7" type="text" name="email">
+            </form>
+          </div>
+          <div class = "button mw9 mt4">
+            <form action="creatorCompany.php" method="post" class="ml3 mr5 pt3 pb2">
+              <label for="password"> password:</label>
+              <input class="bt-0-l br-0-l bl-0-l" type="text" name="password">
+            </form>
+          </div>
+          <div class = "mt4">
+            <form action="creatorCompany.php" method="post">
+              <input class="ph7 pv3 ma0 bckColor black pv2 db bg-animate hover-bg-yellow" type="submit" value="Login">
+            </form>
+          </div>
         </div>
-      <div class="login mt6 pt5 pl3 mh6"> <!--starts the display box that holds the email password and submit button-->
-        <div class = "button mw9">
-          <form action="creatorCompany.php" method="post" class="ml3 mr5 pt3 pb2">
-            <label for="email"> Email:</label>
-            <input class="bt-0-l br-0-l bl-0-l" type="text" name="email">
-          </form>
-        </div>
-        <div class = "button mw9 mt4">
-          <form action="creatorCompany.php" method="post" class="ml3 mr5 pt3 pb2">
-            <label for="password"> password:</label>
-            <input class="bt-0-l br-0-l bl-0-l" type="text" name="password">
-          </form>
-        </div>
-        <div class = "mt4">
-          <form action="creatorCompany.php" method="post">
-            <input class="ph7 pv3 bckColor black pv2 db bg-animate hover-bg-yellow" type="submit" value="Login">
-          </form>
-        </div>
+      </div>
+      <div class = "page2">
+        <p>next page</p>
       </div>
       </div>
     </div>
