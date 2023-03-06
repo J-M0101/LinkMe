2d5c05f1da8b04c22ffe553664c3fea5b0c2999a
Josh <joshua.medina@sjsu.edu>
2023-03-05 21:16:21 -0800

    finished skeleton of index page and got sign in working

---
+++ homepage.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+  <title>Home Page</title>
+  <?php
+    include "head.php";
+?>
+  </head>
+  <body>
+    <div>
+        Hello
+    </div>
+
+    <footer class ="w-100 footer">
+      <div class ="pa3 pb7">
+        
+      </div>
+    </footer>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -1,13 +1,57 @@
 <?php
     session_start();
+    
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+      if (isset($_POST["email"]) && isset($_POST["password"])) 
+      {   
+        $email = $_POST["email"];
+        $password = $_POST["password"];
 
-    // if($_SERVER['REQUEST_METHOD'] === 'POST'){
-    //     if(isset($_POST[""]))
-    // }
+        // Create connection
+        $conn = mysqli_connect("localhost", "root", "", "linkme");
+        
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+
+        $sql = "SELECT password from user WHERE email = '$email'";
+        try{
+            $results = mysqli_query($conn, $sql);
+
+            if ($results) { 
+              $row = mysqli_fetch_assoc($results);
+
+              if ( $row["password"] === $password ) 
+              {
+                  $_SESSION['logged_in'] = true;
+                  $_SESSION['email'] = $_POST["email"];
+                  // TODO: cache username so you don't have to make hella subquries
+                  // $_SESSION['username'] = $row['username'];
+                  // echo "Successful login";
+                  header("Location: homepage.php");
+              }
+              else
+              {
+                  $error_message = 'Incorrect Password';
+              }
+            } 
+          else {
+              $error_message = "Failed Login";
+          }
+        }
+        catch (Exception $e) {
+            $error_message = "Failed query of creditCardNumber and pin";
+        }
+      }
+      else
+      {
+          $error_message = "Missing input";
+      }
+    }
 ?>
 <html>
   <head>
-  <title>Index.html</title>
+  <title>Home Page</title>
   <?php
     include "head.php";
 ?>
@@ -21,7 +65,7 @@
           <p class ="tagLine ma1">Linking Company and Creator</p>
         </div>
         <div class="flex"> <!--display box that holds the email password and submit button-->
-          <form action="profile.php" method="post" class="ml3 mr5 pt3 w-100">
+          <form action="index.php" method="post" class="ml3 mr5 pt3 w-100">
             <div class="flex items-center button mw9 pv2 ph3"><!--display box holds email input-->
                 <label class="sizeOf" for="email"> Email:</label>
                 <input class="w-100" type="text" name="email">
@@ -49,22 +93,24 @@
         </div>
       </div>
     </div>
-    <div class="page">
-      <div class="greybackground flex mt3 mb7 w-40 round">
-          <p class = "ml5 mt5">next page</p>
+    <div class="page justify-around">
+      <div class="greybackground flex w-50 pa5 mr6 mb7 grow dib f3-ns no-underline black pv2 db bg-animate hover-bg-yellow">
+          <p class = "w-100">Test message</p>
       </div>
-      <div class="yellowbackground flex w-40 round">
-          <p class = "ml5 mt5">Test left</p>
+      <div class="yellowbackground flex w-50 pa5 ml6 mt7 grow dib f3-ns no-underline black pv2 db bg-animate hover-bg-yellow">
+          <p class = "w-100">Test message</p>
       </div>
     </div>
-    <div class="page">
-      <div class="w-100">
-        test
+    <div class="page w-100 justify-center pt6">
+      <div class="flex ba align-center items-center justify-center w-50 mb5 round">
+          test
       </div>
     </div>
 
-    <footer class ="w-100 pt7 greybackground">
-      check
+    <footer class ="w-100 footer">
+      <div class ="pa3 pb7">
+        check
+      </div>
     </footer>
   </body>
 </html>
