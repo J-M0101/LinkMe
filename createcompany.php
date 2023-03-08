031ac9896f145f1eec97fc354cef68ce36dc8e14
Josh's mac <joshua.medina@sjsu.edu>
2023-03-08 14:04:00 -0800

    created createcompany page

--- createcompany.php
+++ createcompany.php
@@ -6,9 +6,17 @@
 ?>
   </head>
   <body>
-    <div class="page items-center justify-center ">
-    <a href = "index.php" class ="introTwo">LinkMe</a>
-
+    <div class="flex-column page items-center justify-center">
+    <a href = "index.php" class ="introTwo pb2 flex">LinkMe</a>
+      <div class="flex">
+        <a href="companyinfo.php" class ="flex company mt0 mr4 grow dib f3-ns no-underline">
+          company
+        </a>
+        <div class = "bar3"></div>
+        <a href="creatorinfo.php" class ="company mt0 ml4 grow dib f3-ns no-underline">
+          Creator
+        </a>
+      </div>  
     </div>
   </body>
 </html>
