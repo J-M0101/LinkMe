0f2c54797f3966e9f56e881ba3da3b3633ee37b7
Josh's mac <joshua.medina@sjsu.edu>
2023-03-22 17:12:30 -0700

    Added titlebar/usertabs only html and css

---
+++ creatorinfo.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+  <title>User Type</title>
+  <?php
+    include "head.php";
+?>
+  </head>
+  <body>
+    <div class="flex-column page items-center justify-center">
+      <a href = "index.php" class ="introTwo pb2 ">LinkMe</a>
+  </body>
+</html>
