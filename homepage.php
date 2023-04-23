003900728b378eb223227c8674116263a6d2b4c1
Josh's mac <joshua.medina@sjsu.edu>
2023-04-23 11:43:07 -0700

    Worked on the homepage profile cards ands trending page.

--- homepage.php
+++ homepage.php
@@ -42,7 +42,7 @@
       <!-- middle column -->
       <div class="w-60">
         <!-- search -->
-        <div class="mt4 mb4 " style="text-align: center">
+        <div class="mt4" style="text-align: center">
           <form action="smconnect.php" method="post">
           <label class="pt1" for="search"></label>
           <input class="w-70 pv3 centertext round" type="text" name="search" onfocus="this.value=''" value="search">
@@ -50,12 +50,66 @@
         </div>
         <!-- profile summary -->
         <div class="container">
-          <p class="pv6  yellowbackground">
-            Home Page
-          </p>
-          <p class=" pv6 mt5 yellowbackground">
-            Home Page
-          </p>
+          <!-- First profile box (margin for first is different than the rest) -->
+          <div class="profiletag flex flex-column yellowbackground">
+            <div class="flex flex-column w-100 h-50 pt3 topbox">
+              <div class="flex flex-row justify-around">
+                <div class="flex circle justify-center">
+                  <p class="username">
+                    7.6
+                  </p>
+                </div>
+                <div class="pr6 pt4 username">Username</div>
+              </div>
+            </div>
+            <div class="flex flex-column w-100 h-25 pt1">
+              <div class="username">Statistics</div>
+            </div>
+            <div class="flex  flex-row w-100 h-50 justify-around topbox">
+              <div class="w-100 mediatype">Youtube Name
+                <div class="pt3 pl3 accountname" style="text-align:left">Sum:</div>
+                <div class="pt3 pl3 accountname" style="text-align:left">Engagement:</div>
+              </div>
+              <div class="w-100 mediatype">Facebook Name
+                <div class="pt3 pl3 accountname" style="text-align:left">Sum:</div>
+                <div class="pt3 pl3 accountname" style="text-align:left">Engagement:</div>
+              </div>
+              <div class="w-100 mediatype">Twitter Name
+                <div class="pt3 pl3 accountname" style="text-align:left">Sum:</div>
+                <div class="pt3 pl3 accountname" style="text-align:left">Engagement:</div>
+              </div>
+            </div>
+          </div>
+          <!-- Second profile box. -->
+          <div class="profiletag mt5 flex flex-column yellowbackground">
+            <div class="flex flex-column w-100 h-50 pt3 topbox">
+              <div class="flex flex-row justify-around">
+                <div class="flex circle justify-center">
+                  <p class="username">
+                    7.6
+                  </p>
+                </div>
+                <div class="pr6 pt4 username">Username</div>
+              </div>
+            </div>
+            <div class="flex flex-column w-100 h-25 pt1">
+              <div class="username">Statistics</div>
+            </div>
+            <div class="flex flex-row w-100 h-50 justify-around topbox">
+              <div class="w-100 mediatype">Youtube Name
+                <div class="pt3 pl3 accountname" style="text-align:left">Sum:</div>
+                <div class="pt3 pl3 accountname" style="text-align:left">Engagement:</div>
+              </div>
+              <div class="w-100 mediatype">Facebook Name
+                <div class="pt3 pl3 accountname" style="text-align:left">Sum:</div>
+                <div class="pt3 pl3 accountname" style="text-align:left">Engagement:</div>
+              </div>
+              <div class="w-100 mediatype">Twitter Name
+                <div class="pt3 pl3 accountname" style="text-align:left">Sum:</div>
+                <div class="pt3 pl3 accountname" style="text-align:left">Engagement:</div>
+              </div>
+            </div>
+          </div>
           <p class=" pv6 mt5 yellowbackground">
             Home Page
           </p>
