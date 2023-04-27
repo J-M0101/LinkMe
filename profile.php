ba88dfd2311779d359c43fb8089378f8fbf316ac
Peter Tam <peter.tam@sjsu.edu>
2023-04-26 18:07:12 -0700

    Added stuff to the profile

--- profile.php
+++ profile.php
@@ -10,7 +10,7 @@
     <div class="titlebar">
       <div class="flex justify-between" >
         <div class="w-30">
-          <div class="pt2 logoText" style="witdth: 300px; text-align: center">
+          <div class="pt2 logoText" style="width: 300px; text-align: center">
             <span class = "pl2"><button class="bluebackground homebutton link grow bg-animate" type="button"><a href="homepage.php">LinkMe</a></button> </span><br>
             <span class="tagline">Linking Company and Creator<span>
           </div>
@@ -51,8 +51,30 @@
         <!-- profile summary -->
         <div class="flex h-50 mt4 justify-center">  
           <div class="flex w-90 yellowbackground">
-            
+            <!-- Username -->
+            <div class="mt4 mb4 ml4 mr4" style="text-align: left; font-size: 36px;">
+              John Doe
+            <div>
+            <!-- Icons -->
+            <div class="mt2" style="text-align: left; font-size: 16px; font-family: Arial;">
+              Insert icons here
+            <div>
+            <!-- Line divider -->
+            <div class="mt2">
+            <hr style="border: 2px solid black; margin: 0; width: 100%;">
+            <!-- About me -->
+            <div class="mt2 mb4" style="text-align: left; font-size: 22px;">
+              About Me
+            <div>
+            <div class="mt2 mb4" style="text-align: left; font-size: 16px; font-family: Arial;">
+            I'm a passionate adventurer who loves exploring the great outdoors through mountain climbing, biking, and hiking. With his unwavering enthusiasm and a strong love for nature, John has climbed some of the highest peaks and biked through some of the most challenging trails around the world. When he's not out in the wilderness, John can be found sharing his experiences and knowledge with others, inspiring them to explore and appreciate the beauty of our planet.
+            <div>
+            <!-- Tags -->
+            <div class="mt2 mb4" style="text-align: left; font-size: 16px; font-family: Arial;">
+              Tags: Mountain Climbing, Biking, Hiking
+            <div>
           </div>
+          
         </div>
       </div>
     </div>
